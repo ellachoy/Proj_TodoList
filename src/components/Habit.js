91a343aa7cb4795c6800c729b55c6095c24761d8
Ella Choy <egal21de@gmail.com>
2021-11-02 22:19:59 +0100

    propsComponents

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -15,7 +15,8 @@ class Habit extends Component {
         return (
             <li className="habit">
                 <span className="habit-name">Reading</span>
-               <span className="habit-count">{this.state.count}</span> 
+               <span className="habit-count">{this.state.count}</span>
+                
                <button className="habit-button habit-increase" onClick={this.handleIncrement}>
                <i className="fas fa-plus-square"></i>
                </button>
